a02b96b7f533e8e3e1d6a5c30e8253a322ad909d
JaiBaheti <bahetijay9@gmail.com>
2021-04-28 23:21:53 +0530

    Encryption done

--- motoInsure/src/main/java/com/ibm/motoInsure/service/UserService.java
+++ motoInsure/src/main/java/com/ibm/motoInsure/service/UserService.java
@@ -4,6 +4,8 @@ import com.ibm.motoInsure.bean.Login;
 import com.ibm.motoInsure.entity.User;
 
 public interface UserService {
+	int addUser(User user);
+	String forgotPassword(String uname);
 	int addPolicyToUser(int id,int policyId);
 	int addUserVehicle(int id,String registrationNo);
 	User validate(Login login);
