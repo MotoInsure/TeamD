3fbe118fc45a2163e595a7a6e2c0663fd97466bd
kajal1610185@akgec.ac.in <ibmjavafsdmr09@host.docker.internal>
2021-05-04 11:46:02 +0530

    sms-api

--- motoInsure/src/main/java/com/ibm/motoInsure/service/SMSService.java
+++ motoInsure/src/main/java/com/ibm/motoInsure/service/SMSService.java
@@ -1,6 +1,5 @@
 package com.ibm.motoInsure.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 
