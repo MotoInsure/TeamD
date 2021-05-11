4b2b1effedc61e1f22432ac41bd63ac6b9c2118f
JaiBaheti <bahetijay9@gmail.com>
2021-05-11 13:05:20 +0530

    Vehicle Controller Updated

--- motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleService.java
+++ motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleService.java
@@ -1,7 +1,11 @@
 package com.ibm.motoInsure.service;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
+import com.ibm.motoInsure.entity.Vehicle;
 import com.ibm.motoInsure.entity.VehicleDetails;
 /**
  *  <p>This service interface is for vehicle containing logic foradd and get operations</p>
@@ -16,25 +20,25 @@ public interface VehicleService {
 	
 	
 	
-//	/**
-//	 * 
-//	 * @param vehicle
-//	 * @return id
-//	 */
-//	
-//	int addVehicle(Vehicle vehicle);
-//	/**
-//	 * 
-//	 * @param type
-//	 * @return list of vehicles
-//	 */
-//	List getBrand(String type);
-//	/**
-//	 * 
-//	 * @param Brand
-//	 * @return set of vehicles
-//	 */
-//	Set<String> getModel(String Brand);
+	/**
+	 * 
+	 * @param vehicle
+	 * @return id
+	 */
+	
+	int addVehicle(Vehicle vehicle);
+	/**
+	 * 
+	 * @param type
+	 * @return list of vehicles
+	 */
+	List getBrand(String type);
+	/**
+	 * 
+	 * @param Brand
+	 * @return set of vehicles
+	 */
+	Set<String> getModel(String Brand);
 	
 
 }
--- motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleServiceImpl.java
+++ motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleServiceImpl.java
@@ -21,37 +21,37 @@ import com.ibm.motoInsure.repository.VehicleRepository;
 @Service
 public class VehicleServiceImpl implements VehicleService {
 	@Autowired 
-	private VehicleDetailsRepository vr;
+	private VehicleDetailsRepository vdr;
 	
 	@Override
 	public VehicleDetails getPrice(String registrationNo) {
-		return vr.findByRegistrationNo(registrationNo);
+		return vdr.findByRegistrationNo(registrationNo);
 	}
 	
-//	List<Vehicle> vehicle_list = new ArrayList<>();
-//	@Autowired 
-//	private VehicleRepository vr;
-//	
-//	@Override
-//	public int addVehicle(Vehicle vehicle) {
-//		vr.save(vehicle);
-//		return vehicle.getId();
-//	}
-//	@Override
-//	public List getBrand(String type) {
-//		vehicle_list=vr.findByType(type);
-//		return vehicle_list;
-//	}
-//	@Override
-//	public Set<String> getModel(String Brand) {
-//		HashMap<String, Integer> hashmap = new HashMap();
-//		for (Vehicle vehicle : vehicle_list) 
-//			if(vehicle.getBrand().equalsIgnoreCase(Brand))
-//				hashmap.put(vehicle.getModel(),vehicle.getId());
-//		
-//		return hashmap.keySet();
-//		
-//	}
+	List<Vehicle> vehicle_list = new ArrayList<>();
+	@Autowired 
+	private VehicleRepository vr;
+	
+	@Override
+	public int addVehicle(Vehicle vehicle) {
+		vr.save(vehicle);
+		return vehicle.getId();
+	}
+	@Override
+	public List getBrand(String type) {
+		vehicle_list=vr.findByType(type);
+		return vehicle_list;
+	}
+	@Override
+	public Set<String> getModel(String Brand) {
+		HashMap<String, Integer> hashmap = new HashMap();
+		for (Vehicle vehicle : vehicle_list) 
+			if(vehicle.getBrand().equalsIgnoreCase(Brand))
+				hashmap.put(vehicle.getModel(),vehicle.getId());
+		
+		return hashmap.keySet();
+		
+	}
 	
 	
 }
