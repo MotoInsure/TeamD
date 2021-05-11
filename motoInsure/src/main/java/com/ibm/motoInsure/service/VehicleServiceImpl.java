08e16b74ab7730cb6f35d012e9d032eefbbc1da3
JaiBaheti <bahetijay9@gmail.com>
2021-05-11 12:52:35 +0530

    VehicleController Updated

--- motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleServiceImpl.java
+++ motoInsure/src/main/java/com/ibm/motoInsure/service/VehicleServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm.motoInsure.entity.Vehicle;
+import com.ibm.motoInsure.entity.VehicleDetails;
+import com.ibm.motoInsure.repository.VehicleDetailsRepository;
 import com.ibm.motoInsure.repository.VehicleRepository;
 /**
  * <p>This class is implementation of vehicle service class operations</p>
@@ -18,31 +20,38 @@ import com.ibm.motoInsure.repository.VehicleRepository;
  */
 @Service
 public class VehicleServiceImpl implements VehicleService {
-	
-	List<Vehicle> vehicle_list = new ArrayList<>();
 	@Autowired 
-	private VehicleRepository vr;
+	private VehicleDetailsRepository vr;
 	
 	@Override
-	public int addVehicle(Vehicle vehicle) {
-		vr.save(vehicle);
-		return vehicle.getId();
+	public VehicleDetails getPrice(String registrationNo) {
+		return vr.findByRegistrationNo(registrationNo);
 	}
-	@Override
-	public List getBrand(String type) {
-		vehicle_list=vr.findByType(type);
-		return vehicle_list;
-	}
-	@Override
-	public Set<String> getModel(String Brand) {
-		HashMap<String, Integer> hashmap = new HashMap();
-		for (Vehicle vehicle : vehicle_list) 
-			if(vehicle.getBrand().equalsIgnoreCase(Brand))
-				hashmap.put(vehicle.getModel(),vehicle.getId());
-		
-		return hashmap.keySet();
-		
-	}
+	
+//	List<Vehicle> vehicle_list = new ArrayList<>();
+//	@Autowired 
+//	private VehicleRepository vr;
+//	
+//	@Override
+//	public int addVehicle(Vehicle vehicle) {
+//		vr.save(vehicle);
+//		return vehicle.getId();
+//	}
+//	@Override
+//	public List getBrand(String type) {
+//		vehicle_list=vr.findByType(type);
+//		return vehicle_list;
+//	}
+//	@Override
+//	public Set<String> getModel(String Brand) {
+//		HashMap<String, Integer> hashmap = new HashMap();
+//		for (Vehicle vehicle : vehicle_list) 
+//			if(vehicle.getBrand().equalsIgnoreCase(Brand))
+//				hashmap.put(vehicle.getModel(),vehicle.getId());
+//		
+//		return hashmap.keySet();
+//		
+//	}
 	
 	
 }
